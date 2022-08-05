48f9d70ef88a82edd6d6fc5fa8da8a8fa3abe6bb
Eduardo Romero <eduardoromeror9@outlook.com>
2022-08-05 05:24:09 -0400

    ver las propiedades del usuario autenticado

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { body } from 'express-validator'
-import { admin, crear, guardar, agregarImagen } from '../controllers/propiedadController.js'
+import { admin, crear, guardar, agregarImagen, almacenarImagen } from '../controllers/propiedadController.js'
 import protegerRuta from '../middleware/protegerRuta.js'
 import upload from '../middleware/subirImagen.js'
 
@@ -32,8 +32,9 @@ router.get('/propiedades/agregar-imagen/:id',
 )
 
 router.post('/propiedades/agregar-imagen/:id',
-
-  upload.single('imagen')
+  protegerRuta,
+  upload.single('imagen'),
+  almacenarImagen
 )
 
 
